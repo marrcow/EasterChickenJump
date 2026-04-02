9812b3deb79b3adda4aed283438156f06792c19b
Piotr Krawcow <Piotr.Krawcow@stepstone.com>
2026-04-02 22:49:02 +0200

    Add PWA support: manifest, icons, service worker, iOS meta tags

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'chicken-jump-v1';
+const ASSETS = [
+  '/EasterChickenJump/',
+  '/EasterChickenJump/index.html',
+  '/EasterChickenJump/manifest.json',
+  '/EasterChickenJump/icon.svg',
+  '/EasterChickenJump/icon-192.png',
+  '/EasterChickenJump/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
